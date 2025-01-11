a985dcc3ce69011ad7d2c3b187a749214b592674
Arrk01 <itzabhi1114@gmail.com>
2025-01-11 14:48:15 +0530

    Resume

--- src/components/resume/ResumeButton.tsx
+++ src/components/resume/ResumeButton.tsx
@@ -5,7 +5,7 @@ import { GlowingButton } from '../ui/GlowingButton';
 export function ResumeButton() {
   const handleDownload = () => {
     // Replace with actual resume URL
-    const resumeUrl = '/AbhishekResume.pdf';
+    const resumeUrl = 'https://drive.google.com/file/d/1AHfT4MPDEiqLeioOorfEcYEw4CK90Hdj/view?usp=sharing';
     window.open(resumeUrl, '_blank');
   };
 
